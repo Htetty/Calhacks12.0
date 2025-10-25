c365e919809fbf7b4ff11513075d460bed499039
Htetty <htetswork@gmail.com>
2025-10-24 17:08:33 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "../client/public")));
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, "../client"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running http://localhost:${PORT}`);
+});
